cb4110f7380124ed295f283df2df41281e06d8f3
YouCanBeGod <dallasta.amos@gmail.com>
2023-02-26 13:27:55 -0300

    Using Beans Scopes, Singleton and Prototype

---
+++ src/main/java/dio/springboot/SingletonAndPrototype/SistemaMensagem.java
@@ -0,0 +1,24 @@
+package dio.springboot.SingletonAndPrototype;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SistemaMensagem {
+    @Autowired
+    private Remetente noReply;
+    @Autowired
+    private Remetente techTeam;
+
+    public void enviarConfirmacaoCadastro() {
+        System.out.println(noReply);
+        System.out.println("Seu cadastro foi aprovado");
+    }
+
+    public void enviarMensagemBoasVindas() {
+        techTeam.setEmail("tech@dio.com.br");
+        System.out.println(techTeam);
+        System.out.println("Bem-vindo a Tech Elite");
+    }
+
+}
