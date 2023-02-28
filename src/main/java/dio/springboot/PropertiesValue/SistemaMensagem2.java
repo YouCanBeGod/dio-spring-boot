ca5f74cbeb8ad44860974293ba28f00213a7aae1
YouCanBeGod <dallasta.amos@gmail.com>
2023-02-27 22:22:52 -0300

    Properties configurations

--- src/main/java/dio/springboot/PropertiesValue/SistemaMensagem2.java
+++ src/main/java/dio/springboot/PropertiesValue/SistemaMensagem2.java
@@ -1,5 +1,7 @@
 package dio.springboot.PropertiesValue;
 
+import dio.springboot.SingletonAndPrototype.Remetente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,16 +10,13 @@ import java.util.List;
 @Component
 public class SistemaMensagem2 implements CommandLineRunner {
 
-    @Value("${nome}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones;
-
+    @Autowired
+    private Remetente2 remetente2;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviada por: " + nome + "\nE-mail: " + email + "\n Com telefones para contato: " + telefones);
+        System.out.println("Mensagem enviada por: " + remetente2.getNome()
+                + "\nE-mail: " + remetente2.getEmail()
+                + "\nCom telefones para contato: " + remetente2.getTelefones());
         System.out.println("Seu cadastro foi aprovado /2");
 
     }
